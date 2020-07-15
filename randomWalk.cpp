f06acc12994efeef3cfb60b3e89697c800bb1970
Suzanne Conejos <suzanne@instarlab.org>
2020-07-14 21:23:31 -0400

    Done with mean square displacement and probability.

--- randomWalk.cpp
+++ randomWalk.cpp
@@ -7,102 +7,82 @@
 
 using namespace std;
 
-const int numWalkers = 10;  //NUMBER OF WALKERS
-const int numSteps = 50;  //NUMBER OF STEPS
+const int numWalkers = 100;  //NUMBER OF WALKERS
+const int numSteps = 7;  //NUMBER OF STEPS
 
 int main(int argc, char const* argv[])
 {
-    string line;
-
+    
     //SET OUTPUT TEXT FILES
     ofstream probOut;
-    ofstream posNSteps;
     ofstream posMean;
     probOut.open("positionVSProbabilityOutput.csv");
-    posNSteps.open("positionVSNumStepsOutput.csv");
     posMean.open("positionVSMeanSquareOutput.csv");
 
     cout << "NUMBER OF WALKERS: " << numWalkers << endl;
     cout << "NUMBER OF STEPS: " << numSteps << endl << endl;
 
-    //ARRAY TO HOLD ALL X2AVE VALUES
-    int* x2ave = new int[numSteps]; 
-    for (int i = 0; i < numSteps; i++) {
-        x2ave[i] = 0;
+    //aStepSquared IS WHERE A STEP OF A WALKER IS SQUARED 
+    int* aStepSquared = new int[numSteps]; 
+    for (int itime = 0; itime < numSteps; itime++) {
+        aStepSquared[itime] = 0;
     }
 
     //SET PROBABILITY VARIABLES
-    double* prb = new double[2 * numSteps + 1]; //ARRAY TO HOLD PROBABILITY OF EACH POSITION TO BE VISITED
+    double* probability = new double[2 * numSteps + 1]; //ARRAY TO HOLD PROBABILITY OF EACH POSITION TO BE VISITED
     int* countNumberOfVisits = new int[numSteps * 2 + 1]; //ARRAY TO COUNT THE NUMBER OF VISITS PER POSITION
-    for (int i = 0; i < (2 * numSteps + 1); i++) {
-        countNumberOfVisits[i] = 0;
-        prb[i] = 0.0;
+
+    for (int position = 0; position < (2 * numSteps + 1); position++) {
+        countNumberOfVisits[position] = 0;
     }
 
     //FOR LOOP FOR EACH WALKER
-    for (int k = 0; k < numWalkers; k++) {
+    for (int kWalkr = 0; kWalkr < numWalkers; kWalkr++) {
         int currentPosition = 0; //CURRENT POSITION OF WALKER
 
         //FOR LOOP - FOR EACH STEP TAKEN
-        for (int i = 0; i < numSteps; i++)
+        for (int itime = 0; itime < numSteps; itime++)
         {
-            //TOSS COIN IF HEAD OR TAIL
+            //HEAD OR TAIL CONCEPT
             int randomNum = rand() % 2;
 
-            if (randomNum == 1) //IF HEADS
-            {
-                cout << "Walker " << k + 1 << " Step " << i + 1 << ":" << endl;
-                currentPosition += 1;
-                cout << "==== TOSS COIN SHOWS HEAD: x added so new position x is " << currentPosition << endl;
-            }
-            else //IF TAILS
-            {
-                cout << "Walker " << k + 1 << " Step " << i + 1 << ":" << endl;
-                currentPosition += -1;
-                cout << "==== COIN SHOWS TAIL: x subtracted so new position x is " << currentPosition << endl;
-            }
-
-            //INCREMENT countNumberOfVisits[i] EVERYTIME POSITION i IS VISITED
-            countNumberOfVisits[currentPosition + numSteps] += 1;
+            //==============================
+            if (randomNum == 1)
+                currentPosition++;
+            else 
+                currentPosition--;
+            //==============================
             cout << "==== New position is: " << currentPosition << endl;
 
-            //COMPUTE X2AVE[i]
-            x2ave[i] = x2ave[i] + (currentPosition * currentPosition);
-            cout << "==== The squared distance is <x^2> = " << x2ave[i] << endl << endl;
+            //COMPUTE aStepSquared[itime]
+            aStepSquared[itime] = aStepSquared[itime] + (currentPosition * currentPosition);
+            cout << "==== The squared displacement is x^2 = " << aStepSquared[itime] << endl << endl;
         }
         cout << endl;
+        //INCREMENT countNumberOfVisits[] EVERYTIME currentPosition IS VISITED
+        countNumberOfVisits[currentPosition + numSteps] += 1;
     }
 
     //Mean Square
     cout << endl;
-    for (int i = 0; i < numSteps; i++) {
-        double meanSquare = x2ave[i] / numWalkers;
-        posMean << i << ", " << meanSquare << endl;
+    for (int itime = 0; itime < numSteps; itime++) {
+        double meanSquare = (double)aStepSquared[itime] / (double)numWalkers;
+        posMean << itime << ", " << meanSquare << endl;
     }
 
     //PROBABILITY
     cout << endl;
     cout << "Probability Values:" << endl << endl;
-    for (int i = 0; i < (2 * numSteps + 1); i++)
+    for (int position = 0; position < (2 * numSteps + 1); position++)
     {
         //COMPUTE PROBABILITY OF POSITION i
-        prb[i] = (double)countNumberOfVisits[i] / (double)numWalkers;
-        if (countNumberOfVisits[i] != 0) {
-            cout << "Total Steps and Probability of Walker To Be At Distance " << i - numSteps << ": " << countNumberOfVisits[i] << ", " << prb[i] << endl;
+        probability[position] = (double)countNumberOfVisits[position] / (double)numWalkers;
+        if (countNumberOfVisits[position] != 0) {
+            cout << "Total Steps and Probability of Walker To Be At Distance " << position - numSteps << ": " << countNumberOfVisits[position] << ", " << probability[position];
+            probOut << position - numSteps << ", " << probability[position] << endl;  // Output(x,y): POSITION VS PROBABILITY
         }
-        probOut << i - numSteps << ", " << prb[i] << endl;  // Output(x,y): POSITION VS PROBABILITY
-        posNSteps << i - numSteps << ", " << countNumberOfVisits[i] << endl; // Output(x,y): POSITION VS TOTAL STEPS THE POSITION WAS VISITED
     }
-
-    //THIS IS JUST A REMINDER OF THE THREE OUTPUT FILES CREATED
-    cout << endl
-        << "3 Output files have been created: " << endl
-        << "positionVSProbabilityOutput.csv, " << endl
-        << "positionVSNumStepsOutput.csv, " << endl
-        << "positionVSMeanSquareOutput.csv," << endl << endl;
-
     probOut.close();
-    posNSteps.close();
     posMean.close();
     return 0;
 }
