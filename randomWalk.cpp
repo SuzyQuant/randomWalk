bc456b179e45e0fcfa4e97c16de3826fefe650d2
Suzanne Conejos <suzanne@instarlab.org>
2020-07-18 11:39:15 -0400

    Created a New Probability Loop

--- randomWalk.cpp
+++ randomWalk.cpp
@@ -5,11 +5,14 @@
 #include <cstdlib>
 #include <cmath>
 #include <complex>
+#include <bits/stdc++.h> 
 
 using namespace std;
 
-const int numWalkers = 5;  //NUMBER OF WALKERS
-const int numSteps = 22;  //NUMBER OF STEPS (CANNOT SET GREATER THAN 22 SINCE FACTORIAL IS GOING TO BEHAVE ABNORMALLY)
+const int numWalkers = 1000;  //NUMBER OF WALKERS
+const int numSteps = 22;  //NUMBER OF STEPS (CANNOT SET GREATER THAN 22 SINCE FACTORIAL IS GOING TO BEHAVE ABNORMALLY, MEMORY CAN'T CONTAIN)
+
+double q1 = 1 / sqrt(numSteps), q2 = 1, q3 = sqrt(numSteps);
 
 unsigned long long fact(int n)
 {
@@ -31,10 +34,17 @@ int main(int argc, char const* argv[])
     cout << "NUMBER OF WALKERS: " << numWalkers << endl;
     cout << "NUMBER OF STEPS: " << numSteps << endl << endl;
 
-    //aStepSquared IS WHERE A STEP OF A WALKER IS SQUARED 
+    //aStepSquared IS WHERE A STEP OF A WALKER IS SQUARED
     int* aStepSquared = new int[numSteps];
+    double* cosRq1 = new double[numSteps];
+    double* cosRq2 = new double[numSteps];
+    double* cosRq3 = new double[numSteps];
+
     for (int itime = 0; itime < numSteps; itime++) {
         aStepSquared[itime] = 0;
+        cosRq1[itime] = 0.0;
+        cosRq2[itime] = 0.0;
+        cosRq3[itime] = 0.0;
     }
 
     //SET PROBABILITY VARIABLES
@@ -61,13 +71,53 @@ int main(int argc, char const* argv[])
             else
                 currentPosition--;
             //==============================
+            //TASK TO SHOW NUMERICAL AND ANALYTICAL PART OF x^2 or aStepSquared
             cout << "==== New position is: " << currentPosition << endl;
 
-            //COMPUTE aStepSquared[itime]
-            aStepSquared[itime] = aStepSquared[itime] + (currentPosition * currentPosition);
-            cout << "==== The squared displacement is x^2 = " << aStepSquared[itime] << endl << endl;
+            //COMPUTE aStepSquared[itime] and cosRq1, cosRq2, cosRq3
+            //TASK TO SHOW BOTH NUMERICAL AND ANALYTIC PART OF THE DIFFERENT q's: q1, q2, q3
+            aStepSquared[itime] += (currentPosition * currentPosition);
+            cosRq1[itime] += cos(q1 * currentPosition);
+            cosRq2[itime] += cos(q2 * currentPosition);
+            cosRq3[itime] += cos(q3 * currentPosition);
+
+            //cout << "==== The squared displacement is x^2 = " << aStepSquared[itime] << endl << endl;
+
         }
         cout << endl;
         //INCREMENT countNumberOfVisits[] EVERYTIME currentPosition IS VISITED
         countNumberOfVisits[currentPosition + numSteps] += 1;
     }
+
+    //Mean Square
+    cout << endl;
+    for (int itime = 0; itime < numSteps; itime++) {
+        double meanSquare = (double)aStepSquared[itime] / (double)numWalkers;
+        cosRq1[itime] = (double)cosRq1[itime] / (double)numWalkers;
+        cosRq2[itime] = (double)cosRq2[itime] / (double)numWalkers;
+        cosRq3[itime] = (double)cosRq3[itime] / (double)numWalkers;
+
+        posMean << itime << ", " << meanSquare << ", " << cosRq1[itime] << ", " << cosRq2[itime] << ", " << cosRq3[itime] << endl;
+    }
+
+
+    //GETTING PROBABILITY OF FINAL POSITION
+    //USING FACTORIAL METHOD; ANALYTICAL VERSION
+    //TASK: TO INSERT AGAIN THE PREVIOUS PROBABILITY AND OVERLAP THE VALUES
+    //TO SHOW PLOTS OF PROBABILITY DIST: GAUSSIAN, BINOMIAL (USE EXCEL), NUMERICAL SOLUTION
+    cout << endl;
+    cout << "Probability Values:" << endl << endl;
+
+    for (int position = (-1 * numSteps); position <= numSteps; position+=2)
+    {
+                double totalPossibleVisits = fact(numSteps) / (fact((numSteps + abs(position)) / 2) * fact((numSteps - abs(position)) / 2));
+                double prob = totalPossibleVisits / pow(2, numSteps);
+                probOut << position << ", " << prob << endl;
+    }
+
+    cout << endl << endl;
+
+    probOut.close();
+    posMean.close();
+    return 0;
+}
